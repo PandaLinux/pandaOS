706ffc5bd86cbd20bf08059c2bd91c97cc20d757
Panda Linux <desk.abhimanyu@gmail.com>
2016-01-10 23:50:25 +0530

    Fixed /dev/null not found error

--- phase2/init.sh
+++ phase2/init.sh
@@ -6,6 +6,8 @@ shopt -s -o pipefail
 source "$MOUNT_POINT/common/functions.sh"
 echo 'export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin' >> "$MOUNT_POINT/config"
 
+mkdir -pv $MOUNT_POINT/{dev,proc,sys,run}
+
 _list=(structure linux-api-headers man-pages glibc adjust-toolchain zlib file binutils gmp mpfr mpc gcc bzip2 pkg-config ncurses attr acl libcap sed shadow psmisc procps-ng e2fsprogs coreutils iana-etc m4 flex bison grep readline bash bc libtool gdbm expat inetutils perl xml-parser autoconf automake diffutils gawk findutils gettext intltool gperf groff xz grub less gzip iproute2 kbd kmod libpipeline make patch systemd dbus util-linux man-db tar texinfo vim clean)
 
 for i in ${_list[@]}; do
