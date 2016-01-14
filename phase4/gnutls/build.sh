55e9c77fa91d58aebf19a1086565c5c57bb54344
Panda Linux <desk.abhimanyu@gmail.com>
2016-01-14 12:17:25 +0530

    Update from v3.4.5 to v3.4.3

---
+++ phase4/gnutls/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="gnutls"
+PKG_VERSION="3.4.3"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function prepare() {
+    if [[ ! -f "${TARBALL}" ]]
+    then
+        ln -sv "/source/$TARBALL" "$TARBALL"
+    fi
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {    
+    ./configure --prefix=/usr 		\
+		--without-p11-kit
+    make $MAKE_PARALLEL
+}
+
+function check() {
+    make $MAKE_PARALLEL check
+}
+
+function instal() {
+    make $MAKE_PARALLEL install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
