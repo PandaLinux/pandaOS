b90ff8d8d853cb519ff3e80c11de7a05e3407ee5
Panda Linux <desk.abhimanyu@gmail.com>
2016-01-15 13:03:00 +0530

    Fix small bugs

--- configure.sh
+++ configure.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Setup bash as default shell
-sudo rm -f /bin/sh
-sudo ln -s /bin/bash /bin/sh
-
 # Update the system
 sudo apt-get update && sudo apt-get upgrade --yes --force-yes
 
 # Install GCC 5
 sudo apt-get install --yes --force-yes bash binutils bison bzip2 coreutils diffutils findutils gawk grep gzip m4 make patch perl sed tar texinfo xz-utils
-yes '' | sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test --yes
 sudo apt-get update
 sudo apt-get install --yes --force-yes gcc-5 g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 80 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+
+# Setup bash as default shell
+sudo rm -f /bin/sh
+sudo ln -s /bin/bash /bin/sh
