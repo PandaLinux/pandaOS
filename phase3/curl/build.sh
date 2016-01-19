804d0fa3e0809f5b8dee1fc6c4fb3d1a2bb6228d
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-01-19 22:51:15 +0530

    Added new packages
    curl, wget, openssl for apt

---
+++ phase3/curl/build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="curl"
+PKG_VERSION="7.45.0"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.lzma"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function prepare() {
+    if [[ ! -f "${TARBALL}" ]]
+    then
+        ln -sv "/source/$TARBALL" "$TARBALL"
+    fi
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {    
+    ./configure --prefix=/usr              \
+	            --disable-static           \
+    	        --enable-threaded-resolver &&
+	make $MAKE_PARALLEL    	        
+}
+
+function check() {
+	make $MAKE_PARALLEL test
+}
+
+function instal() {
+    make $MAKE_PARALLEL install &&
+	find docs \( -name Makefile\* -o -name \*.1 -o -name \*.3 \) -exec rm {} \; &&
+	install -v -dm755 /usr/share/doc/curl-7.45.0 &&
+	cp -rv docs/*     /usr/share/doc/curl-7.45.0
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
---
+++ phase3/wget/build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="wget"
+PKG_VERSION="1.16.3"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function prepare() {
+    if [[ ! -f "${TARBALL}" ]]
+    then
+        ln -sv "/source/$TARBALL" "$TARBALL"
+    fi
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {    
+    ./configure --prefix=/usr      \
+    			--with-ssl=openssl \
+	            --sysconfdir=/etc  &&
+	make $MAKE_PARALLEL    	        
+}
+
+function check() {
+	make $MAKE_PARALLEL check
+}
+
+function instal() {
+    make $MAKE_PARALLEL install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
