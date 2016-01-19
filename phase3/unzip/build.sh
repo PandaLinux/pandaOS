afb77a9595ec10e71440879d37bc49812b9bbb2b
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-01-19 16:57:59 +0530

    Added zip

---
+++ phase3/unzip/build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="unzip"
+PKG_VERSION="60"
+
+TARBALL="${PKG_NAME}${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}${PKG_VERSION}"
+
+function prepare() {
+    if [[ ! -f "${TARBALL}" ]]
+    then
+        ln -sv "/source/$TARBALL" "$TARBALL"
+    fi
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {
+	make $MAKE_PARALLEL -f unix/Makefile generic
+}
+
+function check() {
+    echo " "
+}
+
+function instal() {
+	make prefix=/usr MANDIR=/usr/share/man/man1 \
+		 -f unix/Makefile $MAKE_PARALLEL install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
