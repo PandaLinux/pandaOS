b4378c77d714d2c3968c43e6c3bc1ec9ba8eb200
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-01-21 11:32:02 +0530

    Moved Linux PAM and shadow
    Linux PAM is required by packages installed in phase 3.

--- phase4/init.sh
+++ phase4/init.sh
@@ -5,7 +5,7 @@ shopt -s -o pipefail
 
 source "$MOUNT_POINT/common/functions.sh"
 
-_list=(x-setup util-macros xorg-protocol-headers libXau libXdmcp tcl expect dejagnu libffi python2 python3 check libxml2 xcb-proto libxcb freetype harfbuzz freetype font-config xorg-libraries xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm libpciaccess libdrm wayland llvm elfutils mesa xbitmaps libpng xorg-applications xcursor-themes xorg-fonts xkeyboard-config libpciaccess pixman libepoxy linux-pam shadow systemd xorg-server libevdev xorg-evdev-driver xorg-ati-driver xorg-intel-driver twm xterm xclock xinit)
+_list=(x-setup util-macros xorg-protocol-headers libXau libXdmcp tcl expect dejagnu libffi python2 python3 check libxml2 xcb-proto libxcb freetype harfbuzz freetype font-config xorg-libraries xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm libpciaccess libdrm wayland llvm elfutils mesa xbitmaps libpng xorg-applications xcursor-themes xorg-fonts xkeyboard-config libpciaccess pixman libepoxy systemd xorg-server libevdev xorg-evdev-driver xorg-ati-driver xorg-intel-driver twm xterm xclock xinit)
 
 for i in ${_list[@]}; do
     case $i in        		
