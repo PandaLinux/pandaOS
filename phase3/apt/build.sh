69f24377cc200acd34b161699aa1ef1385b4cef5
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-01-26 02:11:43 +0530

    Added apt dependecies

--- phase3/apt/build.sh
+++ phase3/apt/build.sh
@@ -22,18 +22,15 @@ function unpack() {
 }
 
 function build() {
-	pushd buildlib
-		wget -O config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
-		wget -O config.sub 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
-		touch config.rpath
-	popd
+	# this only copies config.{guess,sub}, dislpays some errors and creates a dummy config.rpath
+	touch buildlib/config.rpath
+	automake --add-missing --no-force -W none
 	
-	wget -c http://ftp.debian.org/debian/pool/main/g/gtest/libgtest-dev_1.7.0-3_amd64.deb
-	
-	dpkg -i libgtest-dev_1.7.0-3_amd64.deb
-
-	./configure
-    make $MAKE_PARALLEL
+	./configure --prefix=/usr 			\
+				--sysconfdir=/etc 		\
+				--localstatedir=/var 	\
+				--sbindir=/usr/bin		&&
+	make $MAKE_PARALLEL
 }
 
 function check() {
@@ -41,11 +38,60 @@ function check() {
 }
 
 function instal() {
-	cp -rv bin/apt* /usr/bin &&
-	cp -rv bin/libapt* /usr/lib &&
-	mkdir -pv /etc/apt/{apt.conf.d,sources.list.d,preferences.d} &&
-	mkdir -pv /usr/lib/apt &&
-	cp -rv bin/methods /usr/lib/apt &&
+	# Create all the required directories
+	mkdir -pv /usr/{include/{apt-pkg,apt-private},share/doc/apt/examples,lib/{dpkg/methods/apt,apt/{solvers,methods}}}
+	mkdir -pv /etc/apt/{apt.conf.d,sources.list.d,preferences.d}
+
+	# apt
+	for file in {cache,cdrom,config,get,key,mark}; do
+		install bin/apt-$file /usr/bin/
+	done
+	install bin/apt /usr/bin
+	
+	# apt-utils
+	for file in {extracttemplates,sortpkgs}; do
+		install bin/apt-$file /usr/bin/
+	done
+	
+	# libapt-inst
+	install bin/libapt-inst.so.2.0 /usr/lib
+	ln -srv bin/libapt-inst.so.2.0 /usr/lib/libapt-inst.so.2.0.0
+	ln -srv bin/libapt-inst.so.2.0 /usr/lib/libapt-inst.so
+	
+	# libapt-pkg
+	install bin/libapt-pkg.so.5.0 /usr/lib
+	ln -srv bin/libapt-pkg.so.5.0 /usr/lib/libapt-pkg.so.5.0.0
+	ln -srv bin/libapt-pkg.so.5.0 /usr/lib/libapt-pkg.so
+	
+	# libapt-private
+	install bin/libapt-private.so.0.0 /usr/lib
+	ln -srv bin/libapt-private.so.0.0 /usr/lib/libapt-private.so.0.0.0
+	ln -srv bin/libapt-private.so.0.0 /usr/lib/libapt-private.so
+	
+	# apt solvers
+	install bin/apt-internal-solver /usr/lib/apt/solvers/apt
+	install bin/apt-dump-solver /usr/lib/apt/solvers/dump
+	
+	# apt methods
+	install bin/methods/* /usr/lib/apt/methods
+	install dselect/{install,setup,update} /usr/lib/dpkg/methods/apt
+	install -m 644 dselect/{desc.apt,names} /usr/lib/dpkg/methods/apt	
+	
+	# All example configs
+	install -m 644 doc/examples/* /usr/share/doc/apt/examples
+	
+	# All locales
+	for lang in locale/*; do
+		mkdir -pv /usr/share/$lang/LC_MESSAGES
+		install -m 644 $lang/LC_MESSAGES/* /usr/share/$lang/LC_MESSAGES
+	done
+	
+	# libapt-pkg-dev
+	install -m 644 include/apt-pkg/* /usr/include/apt-pkg
+	
+	# libapt-private-dev
+	install -m 644 include/apt-private/* /usr/include/apt-private
+	
     cat > /etc/apt/sources.list << "EOF"
 ## Begin /etc/apt/sources.list
 
@@ -58,7 +104,7 @@ EOF
 	# Add the sandbox user '_apt'
 	sudo useradd -r -M --system _apt
 
-	sudo wget http://panda-linux.esy.es/ftp/panda/panda-key.gpg -O- | sudo apt-key add -
+	sudo wget -q http://panda-linux.esy.es/ftp/panda/conf/panda.gpg.key -O- | sudo apt-key add -
 	sudo apt update
 }
 
---
+++ phase3/cmake/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="cmake"
+PKG_VERSION="3.4.0"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function prepare() {
+    if [[ ! -f "${TARBALL}" ]]
+    then
+        ln -sv "/source/$TARBALL" "$TARBALL"
+    fi
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {    
+    ./bootstrap --prefix=/usr       \
+	            --system-libs       \
+    	        --mandir=/share/man \
+    	        --no-system-jsoncpp \
+    	        --docdir=/share/doc/${PKG_NAME}-${PKG_VERSION} &&
+	make $MAKE_PARALLEL    	        
+}
+
+function check() {
+	bin/ctest
+}
+
+function instal() {
+    make $MAKE_PARALLEL install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
