ec19dde404ee3bed7f197a5f328da10b8f4b911d
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-15 19:53:03 +0530

    Update
    Remove older gcc package

--- configure.sh
+++ configure.sh
@@ -3,13 +3,14 @@
 # Update the system
 sudo apt-get update && sudo apt-get upgrade --yes --force-yes
 
-# Install GCC 5
+# Install all the required dependencies
 sudo apt-get install --yes --force-yes bash binutils bison bzip2 coreutils diffutils findutils gawk grep gzip m4 make patch perl sed tar texinfo unzip xz-utils
+
+# Install GCC 5
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test --yes
 sudo apt-get update
 sudo apt-get install --yes --force-yes gcc-5 g++-5
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 80 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+sudo apt-get remove --yes --force-yes gcc-4* g++-4*
 
 # Setup bash as default shell
 sudo rm -f /bin/sh
