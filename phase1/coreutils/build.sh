ee695b906c3b5f99d9aa0bfd6f16a263e6a56598
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-21 02:07:32 +0530

    Upgraded to 8.25

--- phase1/coreutils/build.sh
+++ phase1/coreutils/build.sh
@@ -2,9 +2,10 @@
 
 set +h		# disable hashall
 shopt -s -o pipefail
+set -e 		# Exit on error
 
 PKG_NAME="coreutils"
-PKG_VERSION="8.24"
+PKG_VERSION="8.25"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.xz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
