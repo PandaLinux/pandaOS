86a1beada4d58aa256fbff04c546a752a99846c3
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-23 01:05:33 +0530

    Update

--- phase2/clean/build.sh
+++ phase2/clean/build.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 shopt -s -o pipefail
+set -e 		# Exit on error
 
 function clean() {
     /tools/bin/find /{,usr/}{bin,lib,sbin} -type f \
 		    -exec /tools/bin/strip --strip-debug '{}' ';'
 
-    rm -rf /tmp/*
-    rm /usr/lib/lib{bfd,opcodes}.a
-    rm /usr/lib/libbz2.a
-    rm /usr/lib/lib{com_err,e2p,ext2fs,ss}.a
-    rm /usr/lib/libltdl.a
-    rm /usr/lib/libz.a
+    rm -rfv /tmp/*
+    rm -v /usr/lib/lib{bfd,opcodes}.a
+    rm -v /usr/lib/libbz2.a
+    rm -v /usr/lib/lib{com_err,e2p,ext2fs,ss}.a
+    rm -v /usr/lib/libltdl.a
+    rm -v /usr/lib/libz.a
 }
 
 clean;
--- phase2/vim/build.sh
+++ phase2/vim/build.sh
@@ -2,6 +2,7 @@
 
 set +h		# disable hashall
 shopt -s -o pipefail
+set -e 		# Exit on error
 
 PKG_NAME="vim"
 PKG_VERSION="7.4"
@@ -31,9 +32,6 @@ function instal() {
     make $MAKE_PARALLEL install
     
     ln -sv vim /usr/bin/vi
-    for L in  /usr/share/man/{,*/}man1/vim.1; do
-	ln -sv vim.1 $(dirname $L)/vi.1
-    done
     
     cat > /etc/vimrc << "EOF"
 " Begin /etc/vimrc
