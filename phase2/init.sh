2b35b6d12ad3a7211af6a0ce94ca119034316f91
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-23 00:58:33 +0530

    Refactored list

--- phase2/init.sh
+++ phase2/init.sh
@@ -2,13 +2,15 @@
 
 set +h		# disable hashall
 shopt -s -o pipefail
+set -e 		# Exit on error
 
 source "$MOUNT_POINT/common/functions.sh"
 echo 'export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin' >> "$MOUNT_POINT/.config"
+source "$MOUNT_POINT/.config"
 
 mkdir -pv $MOUNT_POINT/{dev,proc,sys,run}
 
-_list=(structure linux-api-headers man-pages glibc adjust-toolchain zlib file binutils gmp mpfr mpc gcc bzip2 pkg-config ncurses attr acl libcap sed shadow psmisc procps-ng e2fsprogs coreutils iana-etc m4 flex bison grep readline bash bc libtool gdbm expat inetutils perl xml-parser autoconf automake diffutils gawk findutils gettext intltool gperf groff xz grub less gzip iproute2 kbd kmod libpipeline make patch systemd dbus util-linux man-db tar texinfo vim clean)
+_list=(structure linux-api-headers man-pages glibc adjust-toolchain zlib file binutils gmp mpfr mpc gcc bzip2 pkg-config ncurses attr acl libcap sed shadow psmisc iana-etc m4 bison flex grep readline bash bc libtool gdbm gperf expat inetutils perl xml-parser intltool autoconf automake xz kmod gettext systemd procps-ng e2fsprogs coreutils diffutils gawk findutils groff grub less gzip iproute2 kbd libpipeline make patch dbus util-linux man-db tar texinfo vim clean)
 
 for i in ${_list[@]}; do
     case $i in
