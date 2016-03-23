2d9d3c96e0d1fe2a2e8c07f9d98684df66404d80
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-23 09:57:08 +0530

    Update
    Added debug mode for easy installation, removed stale functions

--- install.sh
+++ install.sh
@@ -6,89 +6,93 @@ source "${PWD}/common/vars.sh"
 source "${PWD}/common/functions.sh"
 
 configureInstall() {
-    # Remove /tools
-    sudo rm -rf /tools
-    # Remove old .config file
-    rm -f "${PWD}/.config"
-    # Create new .config
-    cat > "${PWD}/.config" << "EOF"
+	# Remove /tools
+	sudo rm -rf /tools
+	# Remove old .config file
+	rm -f "${PWD}/.config"
+	# Create new .config
+	cat > "${PWD}/.config" << "EOF"
 #!/bin/sh
 EOF
 
-      echo -e "Download Packages?"
-      select yn in "Yes" "No"; do
-	  case $yn in
-		Yes ) 
-			# Download All the packages
-			wget --input-file=wget-list --continue --directory-prefix="$PWD/source"
-			pushd "$PWD/source"
-			md5sum -c md5sums
-			popd;
-			break;;
-		No )
-			break;;
-	  esac
-      done
+	echo -e "Download Packages?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) 
+				# Download All the packages
+				wget --input-file=wget-list --continue --directory-prefix="$PWD/source"
+				pushd "$PWD/source"
+				md5sum -c ../md5sums
+				popd;
+				break;;
+			No )
+				break;;
+		esac
+	done
 
-      echo "export MOUNT_POINT=${MOUNT_POINT}" >> "${PWD}/.config"
-    
-      echo -e "Filesystem: "
-      read FILESYSTEM
-      echo "export FILE_SYSTEM=${FILESYSTEM}" >> "${PWD}/.config"
-    
-      echo -e "Turn on checks?"
-      select yn in "Yes" "No"; do
-	  case $yn in
-		Yes )	echo 'export MAKE_CHECK=TRUE' >> "${PWD}/.config";break;;
-		No )	echo 'export MAKE_CHECK=FALSE' >> "${PWD}/.config";break;;
-	  esac
-      done
-        
-      echo -e "Turn on parallelism?"
-      select yn in "Yes" "No"; do
-	  case $yn in
-		Yes )	echo 'export MAKE_PARALLEL="-j$(cat /proc/cpuinfo | grep processor | wc -l)"' >> "${PWD}/.config";break;;
-		No ) 	echo 'export MAKE_PARALLEL=-j1' >> "${PWD}/.config";break;;
-	  esac
-      done
-    
-      echo 'export TARGET="$(uname -m)-panda-linux-gnu"' >> "${PWD}/.config"
-      echo 'export PATH=/tools/bin:/bin:/usr/bin' >> "${PWD}/.config"
-      echo 'export LC_ALL=POSIX' >> "${PWD}/.config"
-      echo 'export VM_LINUZ=vmlinuz-4.2-systemd' >> "${PWD}/.config"
-      echo 'export SYSTEM_MAP=System.map-4.2' >> "${PWD}/.config"
-      echo 'export CONFIG_BACKUP=config-4.2' >> "${PWD}/.config"
-    
-      # Make all the configurations available
-      source "${PWD}/.config"
-      # Mount the filesystem
-      mountFs       
-    
-      # Copy the data
-      echo "Please wait..."
-      echo " "
-      cp -ur ./* "${MOUNT_POINT}"
-      cp -ur "${PWD}"/.config "${MOUNT_POINT}"
-      # Create /tools
-      mkdir "${MOUNT_POINT}/tools"
-      sudo ln -s "${MOUNT_POINT}/tools" /
+	echo "export MOUNT_POINT=${MOUNT_POINT}" >> "${PWD}/.config"
 
-      # Start Installations
-      for i in {1..5}
-      do
-	  cd "$MOUNT_POINT/phase${i}" && bash init.sh
-      done
+	echo -e "Filesystem: "
+	read FILESYSTEM
+	echo "export FILE_SYSTEM=${FILESYSTEM}" >> "${PWD}/.config"
 
-      # Unmount the filesystem
-      umountFs
-   
-      echo -e "Update Grub?"
-      select yn in "Yes" "No"; do
-	  case $yn in
-		Yes ) 	updateGrub;break;;
-		No ) 	break;;
-	  esac
-      done
+	echo -e "Run in Debug Mode?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes )
+				echo -e "Turn on checks?"
+				select yn in "Yes" "No"; do
+					case $yn in
+						Yes )	echo 'export MAKE_CHECK=TRUE' >> "${PWD}/.config";break;;
+						No )	echo 'export MAKE_CHECK=FALSE' >> "${PWD}/.config";break;;
+					esac
+				done
+
+				echo -e "Turn on parallelism?"
+				select yn in "Yes" "No"; do
+					case $yn in
+						Yes )	echo 'export MAKE_PARALLEL="-j$(cat /proc/cpuinfo | grep processor | wc -l)"' >> "${PWD}/.config";break;;
+						No ) 	echo 'export MAKE_PARALLEL=-j1' >> "${PWD}/.config";break;;
+					esac
+				done
+				break ;;
+			No )
+				echo 'export MAKE_PARALLEL="-j$(cat /proc/cpuinfo | grep processor | wc -l)"' >> "${PWD}/.config";
+				echo 'export MAKE_CHECK=FALSE' >> "${PWD}/.config";
+				break ;;
+		esac
+	done
+
+	echo "export TARGET=$TARGET" 		>> "${PWD}/.config"
+	echo "export PATH=$TMP_PATH"		>> "${PWD}/.config"
+	echo "export LC_ALL=$LC_ALL" 		>> "${PWD}/.config"
+	echo "export VM_LINUZ=$VM_LINUZ" 		>> "${PWD}/.config"
+	echo "export SYSTEM_MAP=$SYSTEM_MAP" 	>> "${PWD}/.config"
+	echo "export CONFIG_BACKUP=$CONFIG_BACKUP">> "${PWD}/.config"
+
+	# Make all the configurations available
+	source "${PWD}/.config"
+	# Mount the filesystem
+	mountFs
+
+	# Copy the data
+	echo "Please wait..."
+	echo " "
+	cp -ur ./* "${MOUNT_POINT}"
+	cp -ur "${PWD}"/.config "${MOUNT_POINT}"
+	# Create /tools
+	mkdir "${MOUNT_POINT}/tools"
+	sudo ln -s "${MOUNT_POINT}/tools" /
+	sudo chmod -Rv whoami /tools
+
+	# Start the installation
+	for i in {1..5}
+	do
+		cd "$MOUNT_POINT/phase${i}" && ./init.sh
+	done
+
+	# Unmount the filesystem
+	umountFs
 }
 
 echo "Panda Linux Console Installer"
