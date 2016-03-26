00069c60fdbcb7844d649c807bc3a6344503bdb5
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-27 01:22:35 +0530

    Upgraded to 1.18.1

--- phase4/xorg-server/build.sh
+++ phase4/xorg-server/build.sh
@@ -4,7 +4,7 @@ set +h		# disable hashall
 shopt -s -o pipefail
 
 PKG_NAME="xorg-server"
-PKG_VERSION="1.17.2"
+PKG_VERSION="1.18.1"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
@@ -18,6 +18,9 @@ function unpack() {
 }
 
 function build() {
+	patch -Np1 -i ../${PKG_NAME}-${PKG_VERSION}-add_prime_support-1.patch
+	patch -Np1 -i ../${PKG_NAME}-${PKG_VERSION}-wayland_190-1.patch
+	
 	./configure $XORG_CONFIG         \
 	           --enable-glamor       \
     	       --enable-suid-wrapper \
