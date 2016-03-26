04846a4f2ead2af92f0d8f2f27288a022df6b844
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-03-27 01:21:11 +0530

    Upgraded to 0.34.0

--- phase4/pixman/build.sh
+++ phase4/pixman/build.sh
@@ -2,9 +2,10 @@
 
 set +h		# disable hashall
 shopt -s -o pipefail
+set -e
 
 PKG_NAME="pixman"
-PKG_VERSION="0.32.8"
+PKG_VERSION="0.34.0"
 
 TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
 SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
