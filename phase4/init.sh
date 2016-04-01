01edca86957516c762c9188de8a00840d56885a3
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-04-01 12:46:54 +0530

    Added synaptics driver

--- phase4/init.sh
+++ phase4/init.sh
@@ -5,7 +5,7 @@ shopt -s -o pipefail
 
 source "$MOUNT_POINT/common/functions.sh"
 
-_list=(util-macros xorg-protocol-headers libXau libXdmcp tcl expect dejagnu libffi python2 python3 check libxml2 xcb-proto libxcb pcre glib icu freetype harfbuzz freetype font-config xorg-libraries xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-cursor libpciaccess libdrm wayland llvm elfutils mesa xbitmaps libpng xorg-applications xcursor-themes xorg-fonts libxslt xkeyboard-config pixman libepoxy systemd xorg-server libevdev xorg-evdev-driver linux-kernel xorg-intel-driver xinit)
+_list=(util-macros xorg-protocol-headers libXau libXdmcp tcl expect dejagnu libffi python2 python3 check libxml2 xcb-proto libxcb pcre glib icu freetype harfbuzz freetype font-config xorg-libraries xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xcb-util-cursor libpciaccess libdrm wayland llvm elfutils mesa xbitmaps libpng xorg-applications xcursor-themes xorg-fonts libxslt xkeyboard-config pixman libepoxy systemd xorg-server libevdev xorg-evdev-driver linux-kernel xorg-intel-driver xorg-synaptics-driver xinit)
 
 for i in ${_list[@]}; do
     case $i in
---
+++ phase4/xorg-synaptics-driver/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set +h		# disable hashall
+shopt -s -o pipefail
+
+PKG_NAME="xf86-input-synaptics"
+PKG_VERSION="1.8.3"
+
+TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+
+function prepare() {
+    ln -sv "/source/$TARBALL" "$TARBALL"
+}
+
+function unpack() {
+    tar xf ${TARBALL}
+}
+
+function build() {
+	./configure $XORG_CONFIG &&
+	make $MAKE_PARALLEL
+}
+
+function check() {
+	echo " "
+}
+
+function instal() {
+	make $MAKE_PARALLEL install
+}
+
+function clean() {
+    rm -rf "${SRC_DIR}" "$TARBALL"
+}
+
+clean;prepare;unpack;pushd ${SRC_DIR};build;[[ $MAKE_CHECK = TRUE ]] && check;instal;popd;clean
