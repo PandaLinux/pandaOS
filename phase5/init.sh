fdcaf9499799338e4660ea4955a0e3a48bcca17c
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-04-01 14:32:05 +0530

    Added finalizing steps

--- phase5/init.sh
+++ phase5/init.sh
@@ -5,7 +5,7 @@ shopt -s -o pipefail
 
 source "$MOUNT_POINT/common/functions.sh"
 
-_list=(kf5-setup dbus libevent sqlite shared-mime-info desktop-file-utils ruby gstreamer gst-plugins-base mtdev yasm libjpeg-turbo libtiff libsndfile json-c pulse-audio qt libical bluez boost sgml-common docbook-xml docbook-xsl giflib libgudev modem-manager libndp libnl nspr nss dbus-glib iptables network-manager uri phonon oxygen-fonts lmdb kf5-frameworks konsole cairo pango gdk-pixbuf atk hicolor-icon-theme gtk+2 font-forge at-spi2-core at-spi2-atk libxkbcommon gtk+3 libpwquality qca taglib libdbusmenu-qt libogg libvorbis libcanberra libinput js polkit polkit-qt kf5-plasma sddm)
+_list=(kf5-setup dbus libevent sqlite shared-mime-info desktop-file-utils ruby gstreamer gst-plugins-base mtdev yasm libjpeg-turbo libtiff libsndfile json-c pulse-audio qt libical bluez boost sgml-common docbook-xml docbook-xsl giflib libgudev modem-manager libndp libnl nspr nss dbus-glib iptables network-manager uri phonon oxygen-fonts lmdb kf5-frameworks konsole cairo pango gdk-pixbuf atk hicolor-icon-theme gtk+2 font-forge at-spi2-core at-spi2-atk libxkbcommon gtk+3 libpwquality qca taglib libdbusmenu-qt libogg libvorbis libcanberra libinput js polkit polkit-qt kf5-plasma sddm kfs-finalize)
  
 for i in ${_list[@]}; do
     case $i in    
