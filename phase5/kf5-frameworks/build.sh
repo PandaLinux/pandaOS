d86d50e4e47dbf8898f8b89a6b4361f57ac953e2
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2016-04-01 12:58:30 +0530

    Upgraded to 5.20
    Rewritten code for the new packages

--- phase5/kf5-frameworks/build.sh
+++ phase5/kf5-frameworks/build.sh
@@ -14,107 +14,109 @@ function as_root()
 export -f as_root
 
 function prepare() {
-    cat > frameworks-5.18.0.md5 << "EOF"
-bcbcd88f73333da0ee5fb54f8e5242f1  extra-cmake-modules-5.18.0.tar.xz
-55422b499a3ebc5743df167b26685f3c  attica-5.18.0.tar.xz
-5ce0af21d551fffff36bcf95c2afcfb5  kapidox-5.18.0.tar.xz
-5e6da520d4910dad8a03dde2d5b4c2d2  karchive-5.18.0.tar.xz
-acc299e8013b8fcb0e00d42715570ef3  kcodecs-5.18.0.tar.xz
-061b6eedc15239368e5bd60645b99a50  kconfig-5.18.0.tar.xz
-978c394967cbf1677a6adf75ddc807fe  kcoreaddons-5.18.0.tar.xz
-075272719a8d04a10f9c8d202fc6e5b3  kdbusaddons-5.18.0.tar.xz
-9b5d3628895cc6f09adf47856fc392ab  kdnssd-5.18.0.tar.xz
-d8a16a939ca84b04ec31b4b57a979bd7  kguiaddons-5.18.0.tar.xz
-cbea420a939fedd13f6d4c0b9279b758  ki18n-5.18.0.tar.xz
-5ea44a48edb80307ad19d0fa6dac6384  kidletime-5.18.0.tar.xz
-023449bdce8c1dd8241bafafed7c006a  kimageformats-5.18.0.tar.xz
-a9bbb20d5e0df44afd95f552fd9a116b  kitemmodels-5.18.0.tar.xz
-bb909fcb3e98cfb55082e48d2ca6d669  kitemviews-5.18.0.tar.xz
-57803cd443bc0a1d1c957c71f08e7899  kplotting-5.18.0.tar.xz
-13f7cdb060d00905021f63ce24705219  kwidgetsaddons-5.18.0.tar.xz
-c1a5c3dee013ee189d3279a9e58189e3  kwindowsystem-5.18.0.tar.xz
-5bd6cc807ce152c241aa33a16339710a  networkmanager-qt-5.18.0.tar.xz
-783ceff0aed29a57235fb05abca41b6a  solid-5.18.0.tar.xz
-8fb3c2fac12e7c2671c3318f41bc4161  sonnet-5.18.0.tar.xz
-4abcb4004837be65830b47fd8f2bc6a3  threadweaver-5.18.0.tar.xz
-2732f1ad8224a63fe7a89e7471b6c29c  kauth-5.18.0.tar.xz
-af520088daa15b0ead9fe6077059359b  kcompletion-5.18.0.tar.xz
-6503c2c7b8df59be593f73df87a5efcd  kcrash-5.18.0.tar.xz
-75f85023291440e81cf2fc70e412cd18  kdoctools-5.18.0.tar.xz
-03ff4be4bbb45979c34419a5d05d7cbb  kpty-5.18.0.tar.xz
-a417abc36106520833a6a9b240b874e3  kunitconversion-5.18.0.tar.xz
-ed946b78eb19c3fe1458ec78606d089c  kconfigwidgets-5.18.0.tar.xz
-62bb2ad7e0e98047431274f1cfc66b57  kglobalaccel-5.18.0.tar.xz
-5c4a2ac94e8d1b8f47286eaca83cab0c  kpackage-5.18.0.tar.xz
-8eafea43f9bb4f5e01767df8ebccdc49  kservice-5.18.0.tar.xz
-1c9505a8e516eb01e69049c032772ad8  kdesu-5.18.0.tar.xz
-52afdc09c21c786acbe863e3f64334a3  kemoticons-5.18.0.tar.xz
-7de83ced56231a852956ec41815c22e0  kiconthemes-5.18.0.tar.xz
-fb8c4e9d823b3a8ec084f8c710909147  kjobwidgets-5.18.0.tar.xz
-d7d25c868bad8fb567877924d33770df  knotifications-5.18.0.tar.xz
-2cc0ee770de0714cdec198c23246f28d  ktextwidgets-5.18.0.tar.xz
-f8223c50d9a34f7017edb86bf87341a8  kwallet-5.18.0.tar.xz
-591c050cb901b6484448d2c40d7f0e69  kxmlgui-5.18.0.tar.xz
-66b54cef3432c2a36db1dba8916add93  kbookmarks-5.18.0.tar.xz
-3544f70348993c7fe086854214e48aee  kio-5.18.0.tar.xz
-462e0f48f591d3b1d5c1091a3f5327ae  kdeclarative-5.18.0.tar.xz
-71a5229d1c5648c4cb218a042c1fa018  kcmutils-5.18.0.tar.xz
-82116c14ad16b43ef2fa800b5c48edec  frameworkintegration-5.18.0.tar.xz
-bbf6f2743ffc9ffba7dcb151e7b579e0  kinit-5.18.0.tar.xz
-95cf2acca601bf7d80657d6bb327095b  knewstuff-5.18.0.tar.xz
-ebb657138d4d1a8f73dd0f0656d24450  knotifyconfig-5.18.0.tar.xz
-4d465788cbc34707a64bdebd9aa978bf  kparts-5.18.0.tar.xz
-984c74b7ef242703990caa8b2a0b187e  kactivities-5.18.0.tar.xz
-480a09bea54f2a052a883352a7902e08  kded-5.18.0.tar.xz
-1b4757582112bf05fdc85f4ad8bdeca9  kdewebkit-5.18.0.tar.xz
-6c42319bacb47bc416fde04f57485a72  ktexteditor-5.18.0.tar.xz
-9e03c83e412caf74b42949393665b533  kdesignerplugin-5.18.0.tar.xz
-45573350b9f2257a80beaa81bb1b1115  plasma-framework-5.18.0.tar.xz
-79a4865a6b4c60f180511c35fa1b664e  modemmanager-qt-5.18.0.tar.xz
-88a5ff9ae7d7ea0a87b26312658ba86f  kpeople-5.18.0.tar.xz
-6bbcff1866ff9165dbd0a9b3fd53e763  kxmlrpcclient-5.18.0.tar.xz
-76a2a9ed1dbac45cf4cf50297ba02472  bluez-qt-5.18.0.tar.xz
-62d98eec25d779bad419752793ff88e0  kfilemetadata-5.18.0.tar.xz
-90894895db301b7ab069d272a5b99bdc  baloo-5.18.0.tar.xz
-6663ffe69176c43ae4a4d6aab6f3dc67  breeze-icons-5.18.0.tar.xz
-af3ffb2b0e1f978d13fbdf88da29bb89  oxygen-icons5-5.18.0.tar.xz
-332bd1a8e3d131a5cd7774f3d1092bc3  portingAids/kjs-5.18.0.tar.xz
-ea647553f54e7d25e60cecaa8e3b30ad  portingAids/kdelibs4support-5.18.0.tar.xz
-6c5f6efdd6f69193cfce5c9c57cfeabb  portingAids/khtml-5.18.0.tar.xz
-a3d27a96247c55ea52df34f1cbbc1b2a  portingAids/kjsembed-5.18.0.tar.xz
-2f584b440f68e1f4d97377fd6f9ee930  portingAids/kmediaplayer-5.18.0.tar.xz
-c624dbad90370ed208f728811e2eda91  portingAids/kross-5.18.0.tar.xz
-51f616e04e7e77e0d4f526b1215b33ac  portingAids/krunner-5.18.0.tar.xz
+    cat > frameworks-5.20.0.md5 << "EOF"
+1b2e1a6416f47c3d8b5e1a8bc4680570  extra-cmake-modules-5.20.0.tar.xz
+217141216fa45f79d7373cadd3e3c27c  attica-5.20.0.tar.xz
+7352555cce189863890be9ef8166f665  kapidox-5.20.0.tar.xz
+67f61e0ad2ac41e12597a000a436398b  karchive-5.20.0.tar.xz
+55582ae9c6e021a23bb7c0f25329ea04  kcodecs-5.20.0.tar.xz
+1ee3cc6ae99928adf7fa93204d89a0ce  kconfig-5.20.0.tar.xz
+ae26f693d3c60660b7c593d2f9d293bf  kcoreaddons-5.20.0.tar.xz
+42ec6aa625812307b3710d1d7df517f7  kdbusaddons-5.20.0.tar.xz
+ec54202d940f38866b73c550133072a2  kdnssd-5.20.0.tar.xz
+b2cfb78436d3bf15d2197a0676d4fe19  kguiaddons-5.20.0.tar.xz
+b2c5edb59d6ab4fd78d0bf68cfb631c6  ki18n-5.20.0.tar.xz
+ff732995f09a2fa1168968b84d5b69b5  kidletime-5.20.0.tar.xz
+72be30ed58c068675af34fc3d6430ad0  kimageformats-5.20.0.tar.xz
+9a81d2651bb663b65d5c1671af573a1a  kitemmodels-5.20.0.tar.xz
+6ba8ee40baffbaab3ec81439bdff0fe7  kitemviews-5.20.0.tar.xz
+5507be20fee8389eca24e1358f1273e3  kplotting-5.20.0.tar.xz
+0203c7f7bec440a748e01f24231f3e67  kwidgetsaddons-5.20.0.tar.xz
+67e4026752ad7879de67bc7a6e5b55d0  kwindowsystem-5.20.0.tar.xz
+f25b6f54c7728fc0fb031f01065014f9  networkmanager-qt-5.20.0.tar.xz
+0bcf42d89dc6276dbba3b9f8259d67ab  solid-5.20.0.tar.xz
+3e545a92255a92c6899fc8a8f2aa3439  sonnet-5.20.0.tar.xz
+32400f7753597da9982c36e4ea8d88bb  threadweaver-5.20.0.tar.xz
+fb9c705b84641d62125b878349fcd4db  kauth-5.20.0.tar.xz
+9c8f7777dfd9caddbd07dae52d05e613  kcompletion-5.20.0.tar.xz
+6eb332c832849358e2b8d895fb871784  kcrash-5.20.0.tar.xz
+b4988310d6a97d052a6424ebcf50cfc0  kdoctools-5.20.0.tar.xz
+bf119f71086e84e362a78bc5a798efc4  kpty-5.20.0.tar.xz
+06ee394eca473a0be5532b75abc1b2ee  kunitconversion-5.20.0.tar.xz
+188d0d9cf0a4d865e3efa8bf65004d6d  kconfigwidgets-5.20.0.tar.xz
+52f2ea2af7c86aeadc517ef5058c59ef  kglobalaccel-5.20.0.tar.xz
+8b49da1df16dc5c440c6b5a9b63fddf6  kpackage-5.20.0.tar.xz
+39b08d4a7afff397cb819f6c63de46d6  kservice-5.20.0.tar.xz
+bb1af4f2402e4f1efda63e9815022f85  kdesu-5.20.0.tar.xz
+8c9fd278223ed58f144ef2a69dbeba75  kemoticons-5.20.0.tar.xz
+213a5258476ffc51e77526a31dc909f3  kiconthemes-5.20.0.tar.xz
+6b621bbcb82a3fd8f563298136529b29  kjobwidgets-5.20.0.tar.xz
+d98489d1ef939d5f4a20a2969b93c68d  knotifications-5.20.0.tar.xz
+27ff19e2da61e30b1997121a048c6672  ktextwidgets-5.20.0.tar.xz
+8f7404990e60580dd6b707f6a7c390a4  kwallet-5.20.0.tar.xz
+0faf3974771268f42db7c1d4f52f3b9c  kxmlgui-5.20.0.tar.xz
+bb9a4977c9274119f862bd8fa80b5ff3  kbookmarks-5.20.0.tar.xz
+f700e4089783d2420406b5024fef9f65  kio-5.20.0.tar.xz
+65178a250ab8694885c7727b824266b2  kdeclarative-5.20.0.tar.xz
+917f0d983e9427d6af44f32f8e01977a  kcmutils-5.20.0.tar.xz
+cedac6f1740ca905f2c70cd9202b7e5d  frameworkintegration-5.20.0.tar.xz
+09ce4488755c84d1184ecea5438e2749  kinit-5.20.0.tar.xz
+78b7a63732f76652cc95fed80e138074  knewstuff-5.20.0.tar.xz
+79bf2e28b2bdccdcc6a06c7b0bd2651f  knotifyconfig-5.20.0.tar.xz
+47e856a1cc7fbc3e9ed9f2326e998586  kparts-5.20.0.tar.xz
+7dc889ce8dc8d58fc74270b2462acef7  kactivities-5.20.0.tar.xz
+ecaf1a671b4b8a156b5652da4f6d28be  kded-5.20.0.tar.xz
+6fbaf96bfe290b7a411aae75febe7513  kdewebkit-5.20.0.tar.xz
+4e4cdc9d49dd3a3a865d91ffaec392ba  ktexteditor-5.20.0.tar.xz
+09b2d70bc7b505569a5fc9f35bfed3d2  kdesignerplugin-5.20.0.tar.xz
+8ad1c95b1d1e9a67910341733cc6bf62  plasma-framework-5.20.0.tar.xz
+#02b432683f966a7f56e1ffb90264e88d  modemmanager-qt-5.20.0.tar.xz
+99b17824db5e65963b2419094e51f8d7  kpeople-5.20.0.tar.xz
+5a3823f2e2a2e808784bb87ba4a39448  kxmlrpcclient-5.20.0.tar.xz
+ef9f28cb55851f908e3aa7e5b7198105  bluez-qt-5.20.0.tar.xz
+b5f1f4c3313fd854633d2e1b57784d23  kfilemetadata-5.20.0.tar.xz
+25b72b7e77588d46f4b8e8098679b04e  baloo-5.20.0.tar.xz
+#5b0cbd57e9d2ff7261a5b88fa37f9401  breeze-icons-5.20.0.tar.xz
+#989a432e49ba31a6315c72774af15791  oxygen-icons5-5.20.0.tar.xz
+fbba353023ee472a93c7335d26d681dd  portingAids/kjs-5.20.0.tar.xz
+d7a9c4940be6eca07c8586610e69d586  portingAids/kdelibs4support-5.20.0.tar.xz
+351e8754f3d237aa890a845a6c559093  portingAids/khtml-5.20.0.tar.xz
+c275c599c65bd710499b02b3ed58e79a  portingAids/kjsembed-5.20.0.tar.xz
+a4f35a1b78235e0b047937fbd4953eb0  portingAids/kmediaplayer-5.20.0.tar.xz
+ac7a5cd20b06661edf13faad77d4ac89  portingAids/kross-5.20.0.tar.xz
+99fb57dba31fbf6378528ec364e1e579  portingAids/krunner-5.20.0.tar.xz
 EOF
 }
 
-function check() {
-	wget -r -c -nH --cut-dirs=3 -A '*.xz' -np http://download.kde.org/stable/frameworks/5.18/
-	
-	md5sum -c frameworks-5.18.0.md5
+function check() {	
+	url=http://download.kde.org/stable/frameworks/5.20/
+	wget -c -r -nH --cut-dirs=3 -A '*.xz' -np $url &&
+	sed "s:portingAids/::g" frameworks-5.20.0.md5 | md5sum -c -
 }
 
 function build() {
+	set -e
 	while read -r line; do
-	    # Get the file name, ignoring comments and blank lines
-    	if $(echo $line | grep -E -q '^ *$|^#' ); then continue; fi
-    	file=$(echo $line | cut -d" " -f2)
 
-    	pkg=$(echo $file|sed 's|^.*/||')          # Remove directory
-    	packagedir=$(echo $pkg|sed 's|\.tar.*||') # Package directory
+		# Get the file name, ignoring comments and blank lines
+		if $(echo $line | grep -E -q '^ *$|^#' ); then continue; fi
+		file=$(echo $line | cut -d" " -f2)
 
-    	tar -xf $file
-    	pushd $packagedir
+		pkg=$(echo $file|sed 's|^.*/||')          # Remove directory
+		packagedir=$(echo $pkg|sed 's|\.tar.*||') # Package directory
+
+		tar -xf $file
+		pushd $packagedir
 
 			mkdir build
 			cd    build
 
 			cmake 	-DCMAKE_INSTALL_PREFIX=$KF5_PREFIX \
 					-DCMAKE_PREFIX_PATH=$QT5DIR        \
-		            -DCMAKE_BUILD_TYPE=Release         \
-        		    -DLIB_INSTALL_DIR=lib              \
-        		    -DBUILD_TESTING=OFF                \
-        		    -Wno-dev ..
+					-DCMAKE_BUILD_TYPE=Release         \
+					-DLIB_INSTALL_DIR=lib              \
+					-DBUILD_TESTING=OFF                \
+					-Wno-dev ..
 			make $MAKE_PARALLEL
 			as_root make $MAKE_PARALLEL install
 		popd
@@ -122,7 +124,11 @@ function build() {
 		rm -rf $packagedir
 		as_root /sbin/ldconfig
 
-	done < frameworks-5.18.0.md5
+	done < frameworks-5.20.0.md5
+	set +e
+	
+	mv -v /opt/kf5 /opt/kf5-5.20.0
+	ln -sfvn kf5-5.20.0 /opt/kf5
 }
 
-prepare;check;build;
+prepare;check;build;popd;
